482a44f58ea77441ac9b0d61357b1e6474d8ec4a
Vishnu Vijayan <vvijayan@purdue.edu>
2017-05-27 15:10:18 +0530

    Create add and delete contact functions

--- routes/route.js
+++ routes/route.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const Contact = require('../models/contacts');
+
 //Retrieving data
 router.get('/contacts', (req, res, next) => {
-    res.send('Contact list');
+    Contact.find( (err, contacts) => {
+        res.json(contacts);
+    });
 });
 
 //Add contacts
 router.post('/contact', (req, res, next) => {
-    
+    let newContact = new Contact({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone
+    });
+
+    newContact.save((err, contact) => {
+        if(err) {
+            res.json({msg: 'Failed to add contact'});
+        } else {
+            res.json({msg: 'Contact added successfully'});
+        }
+    })
 });
 
 //Delete Contact
